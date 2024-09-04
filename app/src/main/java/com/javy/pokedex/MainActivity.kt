d3ae1e3ed70835cbabbad4082557e9eccada9aea
javiersearight <javier.searight@gorevel.com>
2024-09-04 14:37:45 -0500

    initial project creation

---
+++ app/src/main/java/com/javy/pokedex/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.javy.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.javy.pokedex.ui.theme.PokedexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PokedexTheme {
+            }
+        }
+    }
+}
